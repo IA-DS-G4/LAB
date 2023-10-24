604d3ab7a0e665b5c55fa88884891d1c0a14b7ce
EliasDSAI <elias.88348@gmail.com>
2023-10-25 00:12:43 +0100

    fix

--- Feature_Extraction_new.py
+++ Feature_Extraction_new.py
@@ -83,8 +83,8 @@ def get_features():
                         print('Using the docker command:' + "docker run -v \"" + docker_save_dir + ":/data" + "\" " + docker_hash + " --input-image-dir \"/data/" + os.path.relpath(patient_dicom_path, docker_save_dir).replace(chr(92),"/") +  "\" --input-seg-file \"/data/" + os.path.relpath(seg_file_path, docker_save_dir).replace(chr(92),"/") + "\" --output-dir \"" + pyradiomics_midsave_path + "\" --volume-reconstructor dcm2niix --features-dict \"/data/" + os.path.relpath(features_dict, docker_save_dir).replace(chr(92),"/") + "\" --temp-dir \"/data/" + os.path.relpath(temp_dir, docker_save_dir).replace(chr(92),"/") + "\" --correct-mask")
                         os.system("docker run -v \"" + docker_save_dir + ":/data" + "\" " + docker_hash + " --input-image-dir \"/data/" + os.path.relpath(patient_dicom_path, docker_save_dir).replace(chr(92),"/") +  "\" --input-seg-file \"/data/" + os.path.relpath(seg_file_path, docker_save_dir).replace(chr(92),"/") + "\" --output-dir \"" + pyradiomics_midsave_path + "\" --volume-reconstructor dcm2niix --features-dict \"/data/" + os.path.relpath(features_dict, docker_save_dir).replace(chr(92),"/") + "\" --temp-dir \"/data/" + os.path.relpath(temp_dir, docker_save_dir).replace(chr(92),"/") + "\" --correct-mask")
                         try:
-                            pyradiomics_midsave_path = r"C:\Users\Diederik\OneDrive\Bureaublad\test\temp file\Features"
-                            testdata = pd.read_csv(r"{}\1.csv".format(pyradiomics_midsave_path))
+
+                            testdata = pd.read_csv(r"C:\Users\Diederik\OneDrive\Bureaublad\test\temp file\Features\1.csv")
                             print(testdata)
                             # append data to features.csv
                             data = df.data.append(testdata)
