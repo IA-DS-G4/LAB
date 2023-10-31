23ca56b3ff43666dd9c93790b0228a9c01eab1a4
Diederik60 <diederikdekker@hotmail.nl>
2023-10-31 15:29:30 +0000

    Update Linear_SVC.py

--- Python scrips/ML pyradiomics/Linear_SVC.py
+++ Python scrips/ML pyradiomics/Linear_SVC.py
@@ -7,17 +7,32 @@ from matplotlib import pyplot as plt
 
 X = pd.read_csv("../../Unsorted/Data files/CSV DATA FILES/ML_Data/X.csv")
 Y = pd.read_csv("../../Unsorted/Data files/CSV DATA FILES/ML_Data/y.csv")
-
-#divide training and test data
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+Y = Y.values.ravel()
 
 #The model
-model = LinearSVC()
-model.fit(X_train, y_train)
-predictions = model.predict(X_test)
-accuracy = accuracy_score(y_test, predictions)
+def LSVC(X,Y,epochs, test_size):
+    e = np.arange(0, epochs)
+    acc_array = np.array([])
+    for i in e:
+        # divide training and test data
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size)
+        model = LinearSVC()
+        model.fit(X_train, y_train)
+        predictions = model.predict(X_test)
+        accuracy = accuracy_score(y_test, predictions)
+        acc_array = np.append(acc_array, accuracy)
+        print("Accuracy:", accuracy)
 
-# Print the accuracy
-print("Accuracy:", accuracy)
+    #make a plot
+    plt.plot(e, acc_array)
+    plt.rcParams['font.size'] = '11'
+    #plt.figure(figsize=(8, 6), dpi=1000)
+    plt.title('Accuracy per epoch ' + "Dataset size: " + str(len(X)))
+    plt.legend()
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.show()
+    return
 
-plt.plot()
+LSVC(X,Y,10, 0.1)
+
