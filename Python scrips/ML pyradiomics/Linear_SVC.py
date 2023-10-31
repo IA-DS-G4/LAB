80384fd7f5d82f469ed51b007dea89f51275e98b
Diederik60 <diederikdekker@hotmail.nl>
2023-10-31 14:54:38 +0000

    Addde machine learning

---
+++ Python scrips/ML pyradiomics/Linear_SVC.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
+from matplotlib import pyplot as plt
+
+X = pd.read_csv(r"C:\Users\Diederik\OneDrive\Bureaublad\studie tn\Minor vakken Porto\IA CAD\GITHUB\LAB\Python scrips\ML pyradiomics\X.csv")
+Y = pd.read_csv(r"C:\Users\Diederik\OneDrive\Bureaublad\studie tn\Minor vakken Porto\IA CAD\GITHUB\LAB\Python scrips\ML pyradiomics\y.csv")
+
+#divide training and test data
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+
+#The model
+model = LinearSVC()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+accuracy = accuracy_score(y_test, predictions)
+
+# Print the accuracy
+print("Accuracy:", accuracy)
+
+plt.plot()
