69dd44876c5146de5f87461ca046b771e1dd8aa7
Diederik60 <diederikdekker@hotmail.nl>
2023-10-31 12:44:59 +0000

    Update Feature_selection.py

--- Python scrips/Feature_selection.py
+++ Python scrips/Feature_selection.py
@@ -20,22 +20,24 @@ columns_to_drop = np.arange(0,11)
 columns_to_drop2 = np.arange(12,30)
 columns_to_drop = np.append(columns_to_drop,  columns_to_drop2)
 columns_to_drop = np.append(columns_to_drop,  [32,33,34,35,36,39,40,41,44,45])
-#tot_data = pr_data.select_dtypes(include=[int, float])
 tot_data.drop(columns=tot_data.columns[columns_to_drop],inplace=True)
-#tot_data.to_csv("../Unsorted/Data files/CSV DATA FILES/backup 29_10/filewip.csv", index=False)
-
+#tot_data = pr_data.select_dtypes(include=[int, float])
+tot_data.to_csv(r"C:\Users\Diederik\OneDrive\Bureaublad\studie tn\Minor vakken Porto\IA CAD\test\backup 29_10\filewip.csv", index=False)
 
 #normalizing data
 def normalize_data(df):
+    header = df.iloc[0]
+    df.drop()
     normalized_data = (df-df.min())/(df.max()-df.min())
     return normalized_data
 
 tot_data_norm = normalize_data(tot_data)
-print(pr_data_norm)
 
 #getting rid of features with low variance
 sel = VarianceThreshold(threshold=0)
 sel.fit_transform(tot_data)
 print(tot_data)
+
+
 
 
