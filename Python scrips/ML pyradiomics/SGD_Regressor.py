3ad6d423c25edfa51490c0c813ff645d1ea7b454
EliasDSAI <elias.88348@gmail.com>
2023-10-31 15:56:20 +0000

    fix

--- Python scrips/ML pyradiomics/SGD_Regressor.py
+++ Python scrips/ML pyradiomics/SGD_Regressor.py
@@ -19,3 +19,32 @@ print(predictions)
 print(predictions_rounded)
 accuracy = accuracy_score(y_test, predictions_rounded)
 print(accuracy)
+
+
+def LSVC(X,Y,epochs, test_size):
+    e = np.arange(0, epochs)
+    acc_array = np.array([])
+    for i in e:
+        # divide training and test data
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size)
+        model.fit(X_train, y_train)
+        predictions = model.predict(X_test)
+        predictions_rounded = [round(num) for num in predictions]
+        accuracy = accuracy_score(y_test, predictions_rounded)
+        acc_array = np.append(acc_array, accuracy)
+        print("Accuracy:", accuracy)
+
+    #make a plot
+    plt.plot(e, acc_array)
+    plt.rcParams['font.size'] = '11'
+    #plt.figure(figsize=(8, 6), dpi=1000)
+    plt.title('Accuracy per epoch ' + "Dataset size: " + str(len(X)))
+    plt.legend()
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    mean_acc = np.mean(acc_array)
+    plt.axhline(mean_acc, color='red', linestyle='dashed', label='Mean accuracy')
+    plt.show()
+    return
+
+LSVC(X,Y,100, 0.1)
