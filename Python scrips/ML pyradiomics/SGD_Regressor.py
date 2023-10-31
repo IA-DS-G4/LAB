99a8dd5f50e60baac48ce6c82eca630271e3cbb5
EliasDSAI <elias.88348@gmail.com>
2023-10-31 15:28:17 +0000

    fix

--- Python scrips/ML pyradiomics/SGD_Regressor.py
+++ Python scrips/ML pyradiomics/SGD_Regressor.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear import SGDRegressor
+from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import accuracy_score
 from matplotlib import pyplot as plt
 
 X = pd.read_csv("../../Unsorted/Data files/CSV DATA FILES/ML_Data/X.csv")
-Y = pd.read_csv("../../Unsorted/Data files/CSV DATA FILES/ML_Data/y.csv")
+Y = pd.read_csv("../../Unsorted/Data files/CSV DATA FILES/ML_Data/y_alt.csv")
 
 #divide training and test data
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1)
 
 model = SGDRegressor()
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
-accuracy = accuracy_score(y_test, predictions)
+predictions_rounded = [round(num) for num in predictions]
+print(predictions)
+print(predictions_rounded)
+accuracy = accuracy_score(y_test, predictions_rounded)
+print(accuracy)
