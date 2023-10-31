399db8d6cbf4ac08dcb7097f589f1fd5b7743a2c
Diederik60 <diederikdekker@hotmail.nl>
2023-10-31 12:36:10 +0000

    updated paths

--- Python scrips/Feature_selection.py
+++ Python scrips/Feature_selection.py
@@ -4,10 +4,10 @@ from sklearn.feature_selection import VarianceThreshold
 
 #-loading data-> backup of 29/10/2023
 #pyradiomics data
-pr_data = pd.read_csv(r"../test/backup 29_10/pyradiomicsBackup.csv")
+pr_data = pd.read_csv("../Unsorted/Data files/CSV DATA FILES/backup 29_10/pyradiomicsBackup.csv")
 
 #total obliteration data
-tot_data = pd.read_csv(r"../test/backup 29_10/total_data_obliterationBackup.csv")
+tot_data = pd.read_csv("../Unsorted/Data files/CSV DATA FILES/backup 29_10/total_data_obliterationBackup.csv")
 
 
 
@@ -20,10 +20,10 @@ tot_data = tot_data.drop(tot_data[tot_data['Malignancy'] == 3].index)
 columns_to_drop = np.arange(1,11)
 columns_to_drop2 =  np.arange(12,30)
 columns_to_drop = np.append(columns_to_drop,  columns_to_drop2)
-columns_to_drop = np.append(columns_to_drop,  [32,33,34,35,36,40,41,42,45,46])
+columns_to_drop = np.append(columns_to_drop,  [32,33,34,35,36,39,40,41,44,45])
 #tot_data = pr_data.select_dtypes(include=[int, float])
 tot_data.drop(columns=tot_data.columns[columns_to_drop],inplace=True)
-tot_data.to_csv(r"../test/backup 29_10/filewip.csv", index=False)
+tot_data.to_csv("../Unsorted/Data files/CSV DATA FILES/backup 29_10/filewip.csv", index=False)
 print(tot_data)
 
 
