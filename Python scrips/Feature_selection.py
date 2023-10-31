481181859f3fe15c40765b5187575b6fc624f3a4
Diederik60 <diederikdekker@hotmail.nl>
2023-10-31 11:35:48 +0000

    Revert "Update Feature_selection.py"
    This reverts commit 5ac70370564148fc472808a961432a6c17eff585, reversing
    changes made to a58ad524bd6cbe4bd412cd24c10a11205288a981.

--- Python scrips/Feature_selection.py
+++ Python scrips/Feature_selection.py
@@ -2,27 +2,9 @@ import pandas as pd
 import numpy as np
 #import scikit-learn
 
-<<<<<<< HEAD
 #-loading data-> backup of 29/10/2023
 #pyradiomics data
 pr_data = pd.read_csv(r"C:\Users\Diederik\OneDrive\Bureaublad\studie tn\Minor vakken Porto\IA CAD\test\backup 29_10\pyradiomicsBackup.csv")
-=======
-data = pd.read_csv("../Unsorted/Data files/intermediate result pydiacom_25_10_2023/total_data_obliterationBackup.csv")
-data = data.select_dtypes(include=[int, float])
-pl_data = pd.read_csv("../Unsorted/Data files/intermediate result pydiacom_25_10_2023/pylidcBackup.csv")
-pl_data= pl_data.select_dtypes(include=[int, float])
-print(pl_data.columns.tolist())
-df = pd.concat([pl_data, data], axis=1)
-dft = df.copy() # make a test copy of the dataframe
-
-
-#moving rows up after the 11th column because its how the feature extractor stores it
-dft.iloc[:, 12:] = df.iloc[:, 11:].values
-
-print(dft.iloc[:, 40:50])
-#print(df)
-
->>>>>>> 92b85242f81cf8b467d99f150a81173a5ed87236
 
 #pylidc data
 pl_data = pd.read_csv("../pylidc/pylidc_csv.csv")
@@ -50,16 +32,8 @@ def normalize_data(df):
     normalized_data = (df-df.min())/(df.max()-df.min())
     return normalized_data
 
-<<<<<<< HEAD
 pr_data_norm = normalize_data(pr_data)
 print(pr_data_norm)
 
 #drop row where malignancy is 3
 #fs_df = fs_df.drop(fs_df[fs_df['malignancy'] == 3].index)
-=======
-fs_df = df
-#fs_df = fs_df.drop(fs_df[fs_df['Malignancy'] == 3].index)
-
-
-df_norm = normalize_data(df)
->>>>>>> 92b85242f81cf8b467d99f150a81173a5ed87236
